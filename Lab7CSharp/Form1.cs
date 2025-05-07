1484c20a9a009ecb98706bdffe7b7b6ca922ca98
Taisiia-Hulko <162361499+Taisiia-Hulko@users.noreply.github.com>
2025-05-07 23:38:03 +0300

    Update Form1.cs
    .

--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Lab7CSharp
 {
@@ -10,19 +9,64 @@ namespace Lab7CSharp
         public Form1()
         {
             InitializeComponent();
+
+            // Прив'язка подій (переконайся, що імена збігаються з Name у дизайнері)
+            buttonInfo.Click += buttonInfo_Click;
+            buttonExit.MouseEnter += buttonExit_MouseEnter;
+            buttonOpen.Click += buttonOpen_Click;
+            buttonSave.Click += buttonSave_Click;
         }
 
+        // Завдання 1: кнопка "Інформація"
         private void buttonInfo_Click(object sender, EventArgs e)
         {
             textBox1.Text = $"X: {this.Location.X}, Y: {this.Location.Y}";
         }
 
+        // Завдання 1: кнопка "Вихід" рухається
         private void buttonExit_MouseEnter(object sender, EventArgs e)
         {
             Random rnd = new Random();
             int newX = rnd.Next(0, this.ClientSize.Width - buttonExit.Width);
             int newY = rnd.Next(0, this.ClientSize.Height - buttonExit.Height);
             buttonExit.Location = new Point(newX, newY);
+        }
+
+        // Завдання 2: кнопка "Відкрити"
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.InitialDirectory = @"C:\Users\mu qing\Desktop\uni\2nd_year\2\крос-платформне\lab7";
+            openDialog.Filter = "Зображення (*.jpg; *.png; *.bmp)|*.jpg;*.png;*.bmp";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                Image original = Image.FromFile(openDialog.FileName);
+                pictureBox1.Image = original;
+            }
+        }
+
+        // Завдання 2: кнопка "Зберегти"
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap original = new Bitmap(pictureBox1.Image);
+                original.RotateFlip(RotateFlipType.RotateNoneFlipX); // Дзеркально по горизонталі
+
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.Filter = "PNG Image|*.png";
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    original.Save(saveDialog.FileName);
+                    MessageBox.Show("Зображення збережено успішно!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Спочатку відкрий зображення.");
+            }
         }
     }
 }
