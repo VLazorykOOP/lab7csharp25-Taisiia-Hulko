41ad5a69b77fe3ae329ff9161d7b11d4be28ed4b
Taisiia-Hulko <162361499+Taisiia-Hulko@users.noreply.github.com>
2025-05-07 23:43:38 +0300

    Update Form1.Designer.cs
    .

--- Lab7CSharp/Form1.Designer.cs
+++ Lab7CSharp/Form1.Designer.cs
@@ -6,6 +6,11 @@ namespace Lab7CSharp
         private System.Windows.Forms.Button buttonInfo;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
 
         protected override void Dispose(bool disposing)
         {
@@ -19,6 +24,21 @@ namespace Lab7CSharp
             this.buttonInfo = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonInfo
@@ -26,6 +46,7 @@ namespace Lab7CSharp
             this.buttonInfo.Location = new System.Drawing.Point(30, 30);
             this.buttonInfo.Name = "buttonInfo";
             this.buttonInfo.Size = new System.Drawing.Size(100, 30);
+            this.buttonInfo.TabIndex = 0;
             this.buttonInfo.Text = "Інформація";
             this.buttonInfo.UseVisualStyleBackColor = true;
             this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
@@ -35,6 +56,7 @@ namespace Lab7CSharp
             this.buttonExit.Location = new System.Drawing.Point(150, 30);
             this.buttonExit.Name = "buttonExit";
             this.buttonExit.Size = new System.Drawing.Size(100, 30);
+            this.buttonExit.TabIndex = 1;
             this.buttonExit.Text = "Вихід";
             this.buttonExit.UseVisualStyleBackColor = true;
             this.buttonExit.MouseEnter += new System.EventHandler(this.buttonExit_MouseEnter);
@@ -42,19 +64,121 @@ namespace Lab7CSharp
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(30, 80);
-            this.textBox1.Size = new System.Drawing.Size(220, 20);
             this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 160);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(317, 161);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(30, 327);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(100, 23);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "Відкрити";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(252, 327);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(95, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Зберегти";
+            this.buttonSave.UseVisualStyleBackColor = true;
+           
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(30, 369);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(353, 161);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+           
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(308, 534);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 7;
+            this.buttonAdd.Text = "Додати";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Location = new System.Drawing.Point(30, 535);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxType.TabIndex = 8;
+           
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(30, 565);
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(121, 22);
+            this.textBoxText.TabIndex = 10;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(157, 535);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(64, 22);
+            this.numericUpDown1.TabIndex = 11;
+        
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(227, 534);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(75, 23);
+            this.buttonColor.TabIndex = 13;
+            this.buttonColor.Text = "Колір";
+            this.buttonColor.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(300, 150);
+            this.ClientSize = new System.Drawing.Size(794, 616);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBoxText);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonInfo);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Lab7";
+         
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
